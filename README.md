a5dc75e27a93850d0acded99ab8e5d837d65ca7c
fahimahammed <fahimahammed.cse@gmail.com>
2023-04-04 16:23:29 +0600

    task added in readme file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Computer Graphics Lab
+1. Draw a National Flag
+2. Hidden Surface Elimination
+3. Translation,Rotation,Scaling
+4. Sphere, Ellipsoid, Torus draw
+5. Superellipsoid, Superellips draw
+6. Bezier curve draw
+7. Bresenham line drawing
+8. Midpoint circle drawing
+9. Cohen Sutherland line clipping algorithm
+10. Sutherland-Hodgeman polygon clipping algorithm
+11. Fractal Geometry
--- national-flag.cpp
+++ national-flag.cpp
@@ -5,28 +5,10 @@
 
 int main()
 {
-//    cout << "Hello world!" << endl;
     int gd = DETECT;
     int gm;
     initgraph(&gd,&gm, (char*)"");
 
-    //circle(100,250,100);
-
-    //setcolor(LIGHTBLUE);
-    // draw rectangle without rectangle function
-    //line(100,250,200,250);
-    //line(100,350,200,350);
-    //line(100,250,100,350);
-    //line(200,250,200,350);
-    //setfillstyle(SOLID_FILL,LIGHTBLUE);
-    //floodfill(101,251,LIGHTBLUE);
-
-    //setcolor(YELLOW);
-    //rectangle(100,300,300,400);
-    //setfillstyle(SOLID_FILL,YELLOW);
-    //floodfill(101,301,YELLOW);
-
-    // draw bd national flag
     setcolor(GREEN);
     rectangle(50,50,250,170);
     setfillstyle(SOLID_FILL,GREEN);
