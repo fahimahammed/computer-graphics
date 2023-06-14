4dfe67d6a63cddda58e65a5998687c27f4a0ed32
fahimahammed <fahimahammed.cse@gmail.com>
2023-06-14 19:57:13 +0600

    national flag with python

--- 1-national-flag.py
+++ 1-national-flag.py
@@ -1,21 +1,18 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-# Create the figure and axis
-fig, ax = plt.subplots(figsize=(10, 6))
+# create the figure and axis
+fig, ax = plt.subplots(figsize = (10, 6))
 
-# Draw the background rectangle
-background = patches.Rectangle((0, 0), 10, 6, facecolor='forestgreen')
+# draw background rectangle 
+background = patches.Rectangle((0, 0), 10, 6, facecolor = 'green')
 ax.add_patch(background)
 
-# Draw the red circle
+# draw circle
 circle = patches.Circle((4.5, 3), radius=2, facecolor='red')
 ax.add_patch(circle)
 
-# Set the limits
 ax.set_xlim(0, 10)
 ax.set_ylim(0, 6)
 
-
-# Display the flag
 plt.show()
