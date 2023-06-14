df56b6cfb77592345a45560fb9c804991dbb0ded
fahimahammed <fahimahammed.cse@gmail.com>
2023-06-15 01:17:51 +0600

    torus

---
+++ 4-3-torus.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+theta = np.linspace(0, 2 * np.pi, 100)
+phi = np.linspace(0, 2 * np.pi, 100)
+theta, phi = np.meshgrid(theta, phi)
+
+R = 2
+r = 1
+x = (R + r * np.cos(theta)) * np.cos(phi)
+y = (R + r * np.cos(theta)) * np.sin(phi)
+z = r * np.sin(theta)
+
+plt.subplot(121)
+plt.title("Torus 2D")
+plt.plot(x, y, 'r.')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid(True)
+plt.axis('equal')
+
+ax = plt.subplot(122, projection='3d')
+ax.set_title("Torus 3D")
+ax.plot_surface(x, y, z, cmap='viridis')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+plt.tight_layout()
+plt.show()
