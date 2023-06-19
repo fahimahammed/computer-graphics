3f626477d9c5ddfdf1bdba6366b9d9dac8e54432
fahimahammed <fahimahammed.cse@gmail.com>
2023-06-19 22:06:53 +0600

    fractal geometry

---
+++ 11-fractal-geometry.py
@@ -0,0 +1,33 @@
+import turtle
+
+def drawSierpinski(length, depth):
+    if depth == 0:
+        # Base case: draw a single triangle
+        for _ in range(3):
+            turtle.forward(length)
+            turtle.left(120)
+    else:
+        # Recursive case: draw three smaller triangles
+        drawSierpinski(length / 2, depth - 1)
+        turtle.forward(length / 2)
+        drawSierpinski(length / 2, depth - 1)
+        turtle.backward(length / 2)
+        turtle.left(60)
+        turtle.forward(length / 2)
+        turtle.right(60)
+        drawSierpinski(length / 2, depth - 1)
+        turtle.left(60)
+        turtle.backward(length / 2)
+        turtle.right(60)
+
+# Set up the turtle graphics window
+turtle.speed(0)  # Fastest speed
+turtle.penup()
+turtle.goto(-200, -200)
+turtle.pendown()
+
+# Call the function to draw the Sierpinski triangle
+drawSierpinski(500, 5)
+
+# Keep the window open until it is manually closed
+turtle.done()
